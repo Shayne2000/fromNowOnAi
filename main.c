d311d3d26af1cfd4c9376c26be63db1bdd525ead
shayne2000 <st3334roongaroon.ac.th@gmail.com>
2025-10-10 18:00:22 +0700

    create C file

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+
+///////////////////////// import file ////////////////////
+
+
+
+
+
+/////////////// read file, show to user and make user select (feture,row,label) ////////// output --> features,label,features_num ////////
+
+
+int features_num ;
+
+
+
+///////////// train test split ///////////
+
+
+
+
+
+////////////// main program /////////////
+
+
+
+
+
+
+///////////// output ////////////////////
+
+return 0;
+
+
+
+}
